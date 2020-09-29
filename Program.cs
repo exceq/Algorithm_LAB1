ff51fc52db44620c61446d80d67ca570d403ac73
M <mihail.petuxov@yandex.ru>
2020-09-29 22:06:56 +0500

    /

--- Program.cs
+++ Program.cs
@@ -11,13 +11,12 @@ namespace algorithm_lab1
         static void Main(string[] args)
         {
             string path = @".\measures\";
-            
+
             var f1 = 1;
             var f2 = 1;
-            for (int i = 1; i < 100000000; i = f1 + f2)
+            for (int i = 1; i < 1000000; i = f1 + f2)
             {
-                //DoSmthAndMeasure(CreateArray(i), path, MyBubbleSort);
-                DoSmthAndMeasure(CreateArray(i), path, Array.Sort);
+                DoSmthAndMeasure(CreateArray(i), path, MyBubbleSort);
                 f1 = f2;
                 f2 = i;
             }
@@ -28,16 +27,16 @@ namespace algorithm_lab1
             sw.Start();
             act(array);
             sw.Stop();
-            var time = sw.ElapsedMilliseconds;
+            var time = sw.ElapsedTicks;
             File.AppendAllText(path + act.Method.Name + ".csv", array.Length + ";" + time + ";\n");
         }
 
-        static void DoSmthAndMeasure<T>(T[] array, string path, Func<T[],T> funk)
+        static void DoSmthAndMeasure<T>(T[] array, string path, Func<T[], T> funk)
         {
             sw.Start();
             funk(array);
             sw.Stop();
-            var time = sw.ElapsedMilliseconds;
+            var time = sw.ElapsedTicks;
             File.AppendAllText(path + funk.Method.Name + ".csv", array.Length + ";" + time + ";\n");
         }
 
@@ -65,13 +64,48 @@ namespace algorithm_lab1
             }
             return max;
         }
+
+        static void MyReverse<T>(T[] array)
+        {
+            var rev = new T[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                rev[i] = array[array.Length - 1 - i];
+            }
+        }
+
+        static T MyBinarySearch<T>(T[] array)
+        {
+            var left = 0;
+            var right = array.Length-1;
+            var searchedValue = 5000;
+
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+                if (searchedValue.CompareTo(array[middle]) == 0)
+                {
+                    return array[middle];
+                }
+                else if (searchedValue.CompareTo(array[middle]) > 0)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+            return  default;
+        }
+
         static int[] CreateArray(int n)
         {
             var rnd = new Random();
             var array = new int[n];
             for (int i = 0; i < n; i++)
             {
-                array[i] = rnd.Next();
+                array[i] = rnd.Next()%10000;
             }
             return array;
         }
